e6c629c6d37b510fb4bc715c668ada21757820fe
Hyguana <daniel.assuncao@fatec.sp.gov.br>
2019-12-01 11:36:58 +0000

    Initial commit

---
+++ Nomes.py
@@ -0,0 +1,6 @@
+name   = input    ('Olá, poderia digitar seu nome e pressinar enter ?: ')
+age    = input    ('Obrigado '+name+' por estar utilizando nosso Software, por favor introduza sua idade?: ')
+weight = input    ('Por favor introduza seu peso (Será Nosso Segredinho ;D): ')
+print             ('Obrigado '+name+' por nos passar suas informações para continuarmos.')
+print             ('Só para confirmar: ')
+print             ('Nome '+name+' Idade: Segredo ;D Peso: Sergredo ;D')
---
+++ nome.py
@@ -0,0 +1,4 @@
+nome='Daniel'
+idade=30
+peso=80.1
+print(nome,idade,peso)
