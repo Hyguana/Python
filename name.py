f64bced5a8ddc13ac23f3ef50476b05a6153845b
DANIEL ASSUNCAO <35895330+Hyguana@users.noreply.github.com>
2021-10-24 22:36:37 +0100

    Create name.py

---
+++ name.py
@@ -0,0 +1,6 @@
+name = input(f'Insira seu nome completo e pressione enter: ')
+print(name)
+if 'Silva' or 'silva' in name:
+    print(f'Olá {name}, Tu faz parte da Família Silva.')
+else:
+    print (f' Olá {name}, Tu não faz parte da família Silva')
